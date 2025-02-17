825d4a6715ada4d2b7e8dd24081e646cdf0698d5
YuriyOrlov <mail.yaorloff@gmail.com>
2025-02-17 19:14:18 +0800

    4 tasks done.

---
+++ homework/yuriy_orlov/Lesson3/task_3.py
@@ -0,0 +1,6 @@
+a = 4
+b = 16
+arithmetic_average = (a + b)/2
+geometric_average = a*b**0.5
+print('Среднее арифметическое равно: ', arithmetic_average)
+print('Среднее геометрическое равно: ', geometric_average)
