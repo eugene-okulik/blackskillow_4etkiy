3e2750b09e12d41ef44d80690f104efe8d69823d
YuriyOrlov <mail.yaorloff@gmail.com>
2025-03-06 22:09:40 +0800

    lesson 5 fixed.

--- homework/yuriy_orlov/Lesson5/task3.py
+++ homework/yuriy_orlov/Lesson5/task3.py
@@ -1,4 +1,5 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 print(
-    f'Students {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}')
+    f'Students {students[0]}, {students[1]}, {students[2]} '
+    f'study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}')
