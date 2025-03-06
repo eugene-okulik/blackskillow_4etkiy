5a97126919e5bb5c7d334220c936b539a5b3c353
YuriyOrlov <mail.yaorloff@gmail.com>
2025-03-06 22:23:28 +0800

    lesson 5 remake.

--- homework/yuriy_orlov/Lesson5/task2.py
+++ homework/yuriy_orlov/Lesson5/task2.py
@@ -1,23 +1,17 @@
-# с помощью среза
 text1 = 'результат операции: 42'
-list_from_text1 = text1.split()
-print(list_from_text1)
-number1 = int(list_from_text1[-1:][0])
+stop1 = text1.index(":") + 2
+print(stop1)
+number1 = int(text1[(stop1):])
 print(number1 + 10)
 
-# с помощью index
 text2 = 'результат операции: 514'
-list_from_text2 = text2.split()
-print(list_from_text2)
-number2 = int(list_from_text2[list_from_text2.index('514')])
+stop2 = text2.index(":") + 2
+print(stop2)
+number2 = int(text2[(stop2):])
 print(number2 + 10)
 
-# с помощью среза и index
 text3 = 'результат работы программы: 9'
-list_from_text3 = text3.split()
-print(list_from_text3)
-srez = list_from_text3[-1:]
-for_index3 = srez[0]
-print(srez, for_index3)
-number3 = int(list_from_text3[list_from_text3.index(for_index3)])
+stop3 = text3.index(":") + 2
+print(stop3)
+number3 = int(text3[(stop3):])
 print(number3 + 10)
