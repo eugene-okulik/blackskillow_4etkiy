9ac0ea9f29b9c44f21a791b36ab803ef0225d25a
YuriyOrlov <mail.yaorloff@gmail.com>
2025-03-28 15:15:35 +0800

    lesson 5 remake2.

--- homework/yuriy_orlov/Lesson5/task3.py
+++ homework/yuriy_orlov/Lesson5/task3.py
@@ -1,5 +1,3 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
-print(
-    f'Students {students[0]}, {students[1]}, {students[2]} '
-    f'study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}')
+print('Students', ', '.join(students), 'study these subjects:', ', '.join(subjects))
