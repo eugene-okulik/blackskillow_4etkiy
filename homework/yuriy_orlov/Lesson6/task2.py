0b1d7ce3acb770b5c9f68a1e2015691ac64e82ce
YuriyOrlov <mail.yaorloff@gmail.com>
2025-04-08 00:25:29 +0800

    lesson 6.

---
+++ homework/yuriy_orlov/Lesson6/task2.py
@@ -0,0 +1,10 @@
+a = range(1, 101)
+for i in a:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FuzzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fuzz")
+    else:
+        (print(i))
