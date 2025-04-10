443a01f7db00e7773429965c9e239cf068b0fc6d
YuriyOrlov <mail.yaorloff@gmail.com>
2025-04-10 21:27:21 +0800

    lesson 7. remake1

--- homework/yuriy_orlov/Lesson7/task3.py
+++ homework/yuriy_orlov/Lesson7/task3.py
@@ -10,3 +10,26 @@ print(int(res1.split()[-1]) + 10, int(res2.split()[-1]) + 10, int(res3.split()[-
 my_tuple = [res1, res2, res3, res4]
 for i in range(0, len(my_tuple)):
     print(int(str(my_tuple[i]).split()[-1]) + 10, end=' ')
+
+
+# Третий вариант через функцию.
+def summa(resultat):
+    print(int(resultat.split()[-1]) + 10, end=' ')
+
+
+print()
+summa(res1)
+summa(res2)
+summa(res3)
+summa(res4)
+
+
+# четвертый вариант через функцию и цикл(меньше строк).
+def summa(resultat):
+    for i in range(0, len(resultat)):
+        print(int(str(resultat[i]).split()[-1]) + 10, end=' ')
+
+
+print()
+my_tuple = [res1, res2, res3, res4]
+summa(my_tuple)
